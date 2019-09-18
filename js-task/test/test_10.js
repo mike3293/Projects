81574312ee7bc7413f43707b47ae31c9147e6ab3
Mikhail Gorodilov <Mikhail.Gorodilov@itechart-group.com>
2019-09-18 13:08:32 +0300

    Add call count in 10-th task

--- js-task/test/test_10.js
+++ js-task/test/test_10.js
@@ -2,14 +2,27 @@ import fp from '../FP_functions.js';
 
 const sum = (a, b) => { return a + b; }
 
-const lazyFold = fp.lazy(fp.fold, [1, 2, 3], sum, 4);
+let FoldCallCount = 0;
+
+function fold(arr, func, initial) {
+    FoldCallCount++;
+    fp.fold(arr, func, initial);
+}
+const lazyFold = fp.lazy(fold, [1, 2, 3], sum, 4);
+
+lazyFold();
+lazyFold();
+lazyFold();
 
 QUnit.test("lazyFold() is a function", function(assert) {
     assert.ok(typeof lazyFold === "function", "lazyFold is a function");
 });
 
+QUnit.test("fold() was called once", function(assert) {
+    assert.equal(FoldCallCount, 1, "FoldCallCount = 1");
+});
 QUnit.test("lazy() work", function(assert) {
-    assert.equal(lazyFold(), fp.fold([1, 2, 3], sum, 4), "result of lazyFold() and fold([1, 2, 3], sum, 4) is the same");
+    assert.equal(lazyFold(), fold([1, 2, 3], sum, 4), "result of lazyFold() and fold([1, 2, 3], sum, 4) is the same");
 });
 
 QUnit.test("lazy() throw errors if args are not ok", function(assert) {
