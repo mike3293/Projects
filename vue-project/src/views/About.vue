7dc5355feb05b3602191d5801b1c52d0cf7dc9a1
Mikhail Gorodilov <Mikhail.Gorodilov@itechart-group.com>
2019-10-09 10:31:51 +0300

    Change version writing method

--- vue-project/src/views/About.vue
+++ vue-project/src/views/About.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h3 v-if="version">version: {{version}}</h3>
+    <h3 v-if="enviroment == 'development'">version: {{version}}</h3>
   </div>
 </template>
 
 <script>
 export default {
   data: function() {
-    return {version: process.env.VUE_APP_VERSION}
+    return {
+      version: process.env.VUE_APP_VERSION,
+      enviroment: process.env.NODE_ENV
+    }
   }
 }
 </script>
