09401f063b3fc480f36103679938f08b79fcaa3b
Mikhail Gorodilov <Mikhail.Gorodilov@itechart-group.com>
2019-11-11 09:24:04 +0300

    Change style of sidebar

--- vue-project/src/admin/Admin.vue
+++ vue-project/src/admin/Admin.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="admin-panel">
+    <!-- <div class="admin-panel">
         <div class="admin-panel__sidebar">
             <md-button
                 class="md-raised admin-panel__link"
@@ -16,62 +16,61 @@
                 :md-ripple="false"
                 to="/admin/surveys"
             >Surveys List</md-button>
-        </div>
+    </div>-->
 
-        <!-- <router-link to="/admin/users">users</router-link> -->
-        <div class="admin-panel__content">
+    <!-- <router-link to="/admin/users">users</router-link> -->
+    <!-- <div class="admin-panel__content">
             <router-view />
         </div>
+    </div>-->
+    <div>
+        <md-app>
+            <md-app-drawer md-permanent="full" :md-swipeable="true" class="admin-nav">
+                <md-button
+                    class="md-raised admin-nav__link"
+                    :md-ripple="false"
+                    to="/admin/users"
+                >Users</md-button>
+                <md-button
+                    class="md-raised admin-nav__link"
+                    :md-ripple="false"
+                    to="/admin/create-survey"
+                >Create survey</md-button>
+                <md-button
+                    class="md-raised admin-nav__link"
+                    :md-ripple="false"
+                    to="/admin/surveys"
+                >Surveys List</md-button>
+            </md-app-drawer>
+
+            <md-app-content>
+                <router-view />
+            </md-app-content>
+        </md-app>
     </div>
 </template>
 
 <style lang="scss">
-.admin-panel {
-    display: flex;
-    &__sidebar {
-        height: 500px;
-        width: 160px;
-        position: relative;
-        z-index: 1;
-        top: 0;
-        left: 0;
-        overflow-x: hidden;
-        border-right: 2px solid;
-        border-bottom: 2px solid;
-    }
-    // &__link {
-    //     // stext-transform: uppercase;
-    //     display: block;
-    //     line-height: 40px;
-    //     height: 40px;
-    //     width: 100px;
-    //     text-align: center;
-    //     color: #fff;
-    //     margin: auto;
-    //     border: solid 1px;
-    //     background-color: #56e480;
-    //     border-color: #99caec;
-    //     font-weight: 700;
-    //     transition: background-color 0.2s;
-    // }
-    // &__link:hover {
-    //     background-color: #48ec50;
-    // }
-    &__content {
-        width: 100%;
-    }
-    &__link {
-        width: 90%;
-        text-transform: none;
-        // font-weight: 500;
-        font-size: 17px;
-        margin: auto;
-        margin-top: 20px;
+.md-app {
+    min-height: 565px;
+    border: 1px solid rgba(#000, 0.12);
+    border-top: none;
+}
 
-        &.router-link-exact-active {
-            color: #1f2eb3 !important;
-            background-color: #f0db1c !important;
-        }
+.md-drawer {
+    width: 150px;
+}
+.admin-nav__link {
+    width: 90%;
+    text-transform: none;
+    // font-weight: 500;
+    font-size: 17px;
+    margin: auto;
+    margin-top: 20px;
+
+    &.router-link-exact-active {
+        color: #1f2eb3 !important;
+        background-color: #f0db1c !important;
     }
 }
 </style>
