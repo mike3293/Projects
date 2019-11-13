5a11851de01b18abf66bd2d2b1f5e7bef5712bda
Mikhail Gorodilov <Mikhail.Gorodilov@itechart-group.com>
2019-11-13 10:09:35 +0300

    Add decorator function to back-end

--- surveys/back-end/functions/index.js
+++ surveys/back-end/functions/index.js
@@ -3,110 +3,143 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-const cors = require("cors")({ origin: "http://localhost:8080" });      // To change
-
 admin.initializeApp(functions.config().firebase);
 
 const auth = admin.auth();
 
 const db = admin.firestore();
 
+const whitelist = ["http://localhost:8080", "https://mike3293.github.io"];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+const cors = require("cors")(corsOptions);      // To change
+
 // @param {Object} req Express Request Object.
 // @param {Object} res Express Response Object
 
 async function edit(req, res) {
-    cors(req, res, async function () {
-        const decodedToken = await auth.verifyIdToken(req.get("Authorization"));
+    const decodedToken = await auth.verifyIdToken(req.get("Authorization"));
 
-        const email = req.body.email;
-        const userAuth = await auth.getUserByEmail(email);
+    const email = req.body.email;
+    const userAuth = await auth.getUserByEmail(email);
 
-        await auth.updateUser(userAuth.uid, {
-            password: req.body.password,
-            email: req.body.newEmail
-        });
-
-        console.log(`${email} edited: ${req.body.newEmail} : ${req.body.password} by ${decodedToken.email}`);
-        res.end();
+    await auth.updateUser(userAuth.uid, {
+        password: req.body.password,
+        email: req.body.newEmail
     });
+
+    console.log(`${email} edited to: ${req.body.newEmail} by ${decodedToken.email}`);
+    res.end();
 }
 
 async function deleteUser(req, res) {
-    cors(req, res, async function () {
-        const decodedToken = await auth.verifyIdToken(req.get("Authorization"));
-        const email = req.body.email;
+    const decodedToken = await auth.verifyIdToken(req.get("Authorization"));
+    const email = req.body.email;
 
-        const userAuth = await auth.getUserByEmail(email);
+    const userAuth = await auth.getUserByEmail(email);
 
-        await auth.deleteUser(userAuth.uid);
+    await auth.deleteUser(userAuth.uid);
 
-        console.log(`${email} deleted by ${decodedToken.email}`);
-        res.end();
-    });
+    console.log(`${email} deleted by ${decodedToken.email}`);
+    res.end();
 }
 
 async function add(req, res) {
-    cors(req, res, async function () {
-        const decodedToken = await auth.verifyIdToken(req.get("Authorization"));
-        const email = req.body.email;
+    const decodedToken = await auth.verifyIdToken(req.get("Authorization"));
+    const email = req.body.email;
 
-        await auth.createUser({
-            password: req.body.password,
-            email
-        });
-
-        console.log(`${email} : ${req.body.password}  by ${decodedToken.email}`);
-
-        res.end();
+    await auth.createUser({
+        password: req.body.password,
+        email
     });
+
+    console.log(`Added ${email}  by ${decodedToken.email}`);
+
+    res.end();
 }
 
 async function getUsers(req, res) {
-    cors(req, res, async function () {
-        await auth.verifyIdToken(req.get("Authorization"));
+    let lastUser = req.body.lastUser;
+    const pageSize = req.body.pageSize;
+    const action = req.body.action;
 
-        let lastUser = req.body.lastUser;
-        const pageSize = req.body.pageSize;
-        const action = req.body.action;
+    console.log(lastUser);
 
-        console.log(lastUser);
+    const usersArray = [];
 
-        const usersArray = [];
+    let Snapshot;
 
-        let Snapshot;
+    if (action === "first") {
+        Snapshot = await db.collection("users").orderBy("login").limit(pageSize).get();
+    }
+    if (action === "next") {
+        Snapshot = await db.collection("users").orderBy("login").startAfter(lastUser).limit(pageSize).get();
+    }
 
-        if (action === "first") {
-            Snapshot = await db.collection("users").orderBy("login").limit(pageSize).get();
+    if (action === "prev") {
+        Snapshot = await db.collection("users").orderBy("login").startAt(lastUser).limit(pageSize).get();
+    }
+
+    for (let user of Snapshot.docs) {
+        const data = user.data();
+        data.id = user.id;
+        usersArray.push(data);
+    }
+
+    console.log(JSON.stringify(usersArray));
+    res.status(200).send(JSON.stringify(usersArray));
+    res.end();
+}
+
+function decorateAllProperties(functions, cors, auth, onRequest) {
+    const decoratedFunctions = {};
+
+    for (let key in functions) {
+        let func = functions[key];
+        let funcWithAuth = async function (req, res) {
+            console.log(req + "first req");
+            try {
+                await auth.verifyIdToken(req.get("Authorization"));
+                // console.log(func);
+                func(req, res);
+            } catch (e) {
+                console.log(e + " first");
+            }
+        };
+
+        // console.log(newFunc);
+        let funcWithAuthWithCors = async function (req, res) {
+            try {
+                // console.log(req);
+                cors(req, res, funcWithAuth.bind(null, req, res));
+            } catch (e) {
+                console.log(e);
+            }
         }
-        if (action === "next") {
-            Snapshot = await db.collection("users").orderBy("login").startAfter(lastUser).limit(pageSize).get();
-        }
 
-        if (action === "prev") {
-            Snapshot = await db.collection("users").orderBy("login").startAt(lastUser).limit(pageSize).get();
-        }
+        let funcWithAuthWithCorsWithOnRequest = onRequest(funcWithAuthWithCors);
 
-        for (let user of Snapshot.docs) {
-            const data = user.data();
-            data.id = user.id;
-            usersArray.push(data);
-        }
+        decoratedFunctions[key] = funcWithAuthWithCorsWithOnRequest;
+    }
 
-        console.log(JSON.stringify(usersArray));
-        res.status(200).send(JSON.stringify(usersArray));
-        res.end();
-    });
+    return decoratedFunctions;
 }
 
 const exportedFunctions = {
     delete: deleteUser,
     edit: edit,
     add: add,
-    get: functions.https.onRequest(getUsers)
+    get: getUsers
 };
 
+const exportedFunctionsWithAuthWithCorsWithOnRequest = decorateAllProperties(exportedFunctions, cors, auth, functions.https.onRequest);
 
-const exportedFunctionsWithOnRequestWithCorsWithAuth = decorateAllProperties(exportedFunctions, functions.https.onRequest, cors, auth);
-
-
-module.exports = exportedFunctionsWithOnRequestWithCorsWithAuth;
+module.exports = exportedFunctionsWithAuthWithCorsWithOnRequest;
