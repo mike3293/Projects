9292d6b3c323b840d7c7f334b57e3ed7000e8e4b
Mikhail Gorodilov <Mikhail.Gorodilov@itechart-group.com>
2019-11-19 11:19:21 +0300

    Improve validation for sign-up page

--- surveys/front-end/src/auth/SignUp.vue
+++ surveys/front-end/src/auth/SignUp.vue
@@ -1,42 +1,66 @@
 <template>
     <div class="sign-up">
-        <form v-on:submit.prevent="signup(login, password, name)" class="sign-up__form">
-            <h3 class="sign-up__title">Sign up</h3>
-            <input required v-model="name" placeholder="Name" class="sign-up__input" />
-            <div class="sign-up__errors"></div>
-            <input
-                required
-                v-model="login"
-                placeholder="Login (email)"
-                type="email"
-                class="sign-up__input"
-            />
-            <div class="sign-up__errors"></div>
-            <input
-                v-model.trim="$v.password.$model"
-                placeholder="Password"
-                class="sign-up__input"
-                type="password"
-            />
-            <div class="sign-up__errors">
-                <p class="error" v-if="!$v.password.required">this field is required</p>
-                <p
-                    class="error"
-                    v-if="!$v.password.minLenght"
-                >password must have at least 6 letters.</p>
-            </div>
-            <input
-                v-model.trim="$v.repeatPassword.$model"
-                placeholder="Repeat password"
-                class="sign-up__input"
-                v-bind:class="{ notSame: !$v.repeatPassword.sameAsPassword }"
-                type="password"
-            />
-            <div class="sign-up__errors">
-                <p class="error" v-if="!$v.repeatPassword.sameAsPassword">not same</p>
-            </div>
+        <form novalidate v-on:submit.prevent="validateUser" class="sign-up__form">
+            <md-card>
+                <md-card-header>
+                    <div class="md-title">Sign up</div>
+                </md-card-header>
+                <input v-model="name" placeholder="Name" type="username" class="sign-up__input" />
+                <div class="sign-up__errors">
+                    <p
+                        class="error"
+                        v-if="!$v.name.required && getValidation('name')"
+                    >this field is required</p>
+                </div>
+                <input
+                    required
+                    v-model="email"
+                    placeholder="Email"
+                    type="email"
+                    class="sign-up__input"
+                />
+                <div class="sign-up__errors">
+                    <div v-if="getValidation('email')">
+                        <p class="error" v-if="!$v.email.required">The email is required</p>
+                        <p class="error" v-if="!$v.email.email">Invalid email</p>
+                    </div>
+                </div>
+                <input
+                    v-model.trim="$v.password.$model"
+                    placeholder="Password"
+                    class="sign-up__input"
+                    autocomplete="new-password"
+                    type="password"
+                />
+                <div class="sign-up__errors">
+                    <div v-if="getValidation('password')">
+                        <p class="error" v-if="!$v.password.required">this field is required</p>
+                        <p
+                            class="error"
+                            v-if="!$v.password.minLenght"
+                        >password must have at least 6 letters.</p>
+                    </div>
+                </div>
+                <input
+                    v-model.trim="$v.repeatPassword.$model"
+                    placeholder="Repeat password"
+                    class="sign-up__input"
+                    v-bind:class="{ notSame: !$v.repeatPassword.sameAsPassword && getValidation('repeatPassword') }"
+                    type="password"
+                />
+                <div class="sign-up__errors">
+                    <p
+                        class="error"
+                        v-if="!$v.repeatPassword.sameAsPassword && getValidation('repeatPassword')"
+                    >not same</p>
+                </div>
 
-            <button class="sign-up__button button" :disabled="isLoading || check">Sign up</button>
+                <md-button
+                    type="submit"
+                    class="md-raised sign-up__button button"
+                    :disabled="isLoading"
+                >Sign up</md-button>
+            </md-card>
         </form>
     </div>
 </template>
@@ -50,18 +74,19 @@
 }
 .sign-up {
     &__form {
-        max-width: 380px;
+        max-width: 400px;
         display: block;
-        border: 2px solid black;
         margin: 100px auto;
     }
 
     &__input {
         width: 70%;
+        border-radius: 5px;
+        height: 25px;
     }
+
     &__links {
         margin: auto;
-        margin-bottom: 5px;
         width: 70%;
         display: flex;
         justify-content: space-between;
@@ -86,20 +111,27 @@
 </style>
 
 <script>
-import { required, minLength, sameAs } from "vuelidate/lib/validators";
+import { required, minLength, sameAs, email } from "vuelidate/lib/validators";
 import { mapState } from "vuex";
 
 export default {
     name: "sign-up",
     data: function() {
         return {
-            login: "",
+            email: "",
             password: "",
             repeatPassword: "",
             name: ""
         };
     },
     validations: {
+        name: {
+            required
+        },
+        email: {
+            required,
+            email
+        },
         password: {
             required,
             minLenght: minLength(6)
@@ -118,7 +150,21 @@ export default {
         }
     },
     methods: {
-        async signup(...args) {
+        getValidation(fieldName) {
+            const field = this.$v[fieldName];
+
+            if (field) {
+                return field.$invalid && field.$dirty;
+            }
+        },
+        async validateUser() {
+            this.$v.$touch();
+
+            if (!this.$v.$invalid) {
+                this.signUp(this.email, this.password, this.name);
+            }
+        },
+        async signUp(...args) {
             try {
                 this.$store.commit("common/setLoading", true);
 
