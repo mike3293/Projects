1e90fa6ce4569c1de3aabe0b989714707b538085
Mikhail Gorodilov <Mikhail.Gorodilov@itechart-group.com>
2019-11-19 10:51:44 +0300

    Add validation to sign-in page

--- surveys/front-end/src/auth/SignIn.vue
+++ surveys/front-end/src/auth/SignIn.vue
@@ -1,71 +1,133 @@
 <template>
     <div class="sign-in">
-        <form v-on:submit.prevent="signIn(login, password)" class="sign-in__form">
-            <h3 class="sign-in__title">Sign in</h3>
-            <input
-                required
-                v-model="login"
-                autocomplete="username"
-                type="email"
-                placeholder="Email"
-                class="sign-in__input"
-            />
-            <input
-                required
-                v-model="password"
-                autocomplete="current-password"
-                placeholder="Password"
-                class="sign-in__input"
-                type="password"
-            />
-            <div class="sign-in__links">
-                <router-link to="/sign-up">Sign up</router-link>
-                <router-link to="/">Password?</router-link>
-            </div>
-            <button class="sign-in__button button" :disabled="isLoading">Sign in</button>
+        <form novalidate class="sign-in__form" @submit.prevent="validateUser">
+            <md-card>
+                <md-card-header>
+                    <div class="md-title">Sign in</div>
+                </md-card-header>
+
+                <md-card-content class="md-layout md-alignment-center">
+                    <input
+                        autocomplete="username"
+                        v-model="email"
+                        :disabled="isLoading"
+                        placeholder="Email"
+                        class="sign-in__input"
+                    />
+                    <div class="sign-in__errors">
+                        <div v-if="getValidation('email')">
+                            <p class="error" v-if="!$v.email.required">The email is required</p>
+                            <p class="error" v-if="!$v.email.email">Invalid email</p>
+                        </div>
+                    </div>
+                    <input
+                        v-model="password"
+                        autocomplete="current-password"
+                        placeholder="Password"
+                        class="sign-in__input"
+                        type="password"
+                    />
+                    <div class="sign-in__errors">
+                        <div v-if="getValidation('password')">
+                            <p class="error" v-if="!$v.password.required">this field is required</p>
+                            <p
+                                class="error"
+                                v-if="!$v.password.minLenght"
+                            >password must have at least 6 letters.</p>
+                        </div>
+                    </div>
+                </md-card-content>
+
+                <div class="sign-in__links">
+                    <router-link to="/sign-up">Sign up</router-link>
+                    <md-button
+                        type="submit"
+                        class="md-raised"
+                        :disabled="isLoading"
+                        :md-ripple="false"
+                    >Sign in</md-button>
+                </div>
+            </md-card>
         </form>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @import "../shared/components/button.css";
+.error {
+    margin: 0;
+    padding: 0;
+    color: red;
+}
 .sign-in {
     &__form {
-        max-width: 380px;
+        max-width: 400px;
         display: block;
-        border: 2px solid black;
         margin: 100px auto;
     }
 
     &__input {
         width: 70%;
-        margin-bottom: 20px;
+        border-radius: 5px;
+        height: 25px;
     }
     &__links {
         margin: auto;
-        margin-bottom: 5px;
+        padding-bottom: 10px;
         width: 70%;
         display: flex;
         justify-content: space-between;
+        align-items: center;
     }
 
-    &__button {
-        margin: 10px auto;
+    &__errors {
+        display: block;
+        height: 20px;
+        line-height: 20px;
+        text-align: left;
+        width: 70%;
+        margin: auto;
     }
 }
 </style>
 
 <script>
+import { required, minLength, email } from "vuelidate/lib/validators";
 import { mapState } from "vuex";
+
 export default {
     name: "sign-in",
     data: function() {
         return {
-            login: "",
+            email: "",
             password: ""
         };
     },
+    validations: {
+        email: {
+            required,
+            email
+        },
+        password: {
+            required,
+            minLenght: minLength(6)
+        }
+    },
     methods: {
+        getValidation(fieldName) {
+            const field = this.$v[fieldName];
+
+            if (field) {
+                return field.$invalid && field.$dirty;
+            }
+        },
+        async validateUser() {
+            this.$v.$touch();
+
+            if (!this.$v.$invalid) {
+                this.signIn(this.email, this.password);
+            }
+        },
         async signIn(...args) {
             try {
                 this.$store.commit("common/setLoading", true);
